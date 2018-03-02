9ac4090a8490202fb76859aa0b27373389c67735
Anna <annadmitrievna1823@gmail.com>
2018-03-02 13:12:51 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+    let zerosCount = number;
+    let nBase = base;
+
+    let z = 0;
+    let k = 0;
+    let useNum = 0;
+
+    for (let i = 2; i <= base; i++) {
+         k = 0;
+         useNum = number;
+
+        if (nBase % i === 0) {
+             z = 0;
+
+            while (nBase % i === 0) {
+                z++;
+                nBase = Math.floor(nBase/i);
+            }
+
+            while (useNum/i > 0) {
+                k = k + Math.floor(useNum/i);
+                useNum = Math.floor(useNum/i);
+            }
+
+            zerosCount = Math.floor(Math.min(zerosCount, k/z));
+        }
+    }
+    return zerosCount;
+};
+
